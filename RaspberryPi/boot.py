79a227fb9e5ce5879350bf13d97f881d13c46b14
HOCProj <HOCProj@gmail.com>
2017-05-01 13:10:06 -0500

    Update boot.py

--- RaspberryPi/boot.py
+++ RaspberryPi/boot.py
@@ -1,10 +1,12 @@
-#This script tweets a random number and prints some diagnostic information. Place in /etc/profiles to run on login
 #!usr/bin/python
 print("Boot script started")
 
 import random
 import datetime
+import os
+import socket
 from twython import Twython
+
 print("Twython imported successfully.")
 #auth.py is a copy of auth.py in /home/pi/twitter
 from auth import (
@@ -24,3 +26,23 @@ print("Sending boot tweet...")
 api=Twython(conKey,conSec,accTok,accSec)
 api.update_status(status=tweetStr)
 print("Boot tweet sent successfully")
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+ip = get_ip()
+target=open("ipaddress.txt",'w')
+target.truncate()
+target.write(ip)
+target.close()
+print("calling emailip script...")
+os.system("bash emailip")
